1f836674ba13b1d9f93388c64dd381f337898dea
punkfruit <60442787+punkfruit@users.noreply.github.com>
2023-09-11 12:15:59 -0600

    Create lab1.cs

---
+++ lab1.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int number = 6;
+      string name = "Billy";
+      double secondNumber = 9D;
+      Console.WriteLine(name + " is " + number + secondNumber + " Years old");
+     }
+  }
+}
