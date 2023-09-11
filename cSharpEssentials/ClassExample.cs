69ac3bae7df0a260c4b4e5dcccc8c0acd4751bdb
punkfruit <thecybergrape@gmail.com>
2023-09-11 12:51:23 -0600

    initial commit

---
+++ cSharpEssentials/ClassExample.cs
@@ -0,0 +1,32 @@
+//Use this file to store your work to demonstrate your understanding of operators
+//Make sure your work is different from the instructors examples or your peers
+using System;
+
+public class ClassExample
+{
+    
+}
+
+public class ClassWithVariables
+{
+    public int number = 2;
+    public string userName = "Bob";
+
+    public string girlfriend = "Jessica";
+    public int girlfriendAge = 25;
+}
+
+public class ClassWithFunctions
+{
+    public void RunFunction()
+    {
+        Console.WriteLine("Running Function");
+    }
+
+    public int AnotherFunction()
+    {
+        Console.WriteLine("this is another function! yay!");
+        return 1;
+
+    }
+}   
