ab20f7ac50190e0a9718e21c3faa48d876d4baae
punkfruit <thecybergrape@gmail.com>
2023-09-13 14:01:24 -0600

    big ol update

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+					
+public class TemperatureCheck
+{
+	public static void Main()
+	{
+		Console.WriteLine("Enter the current temperature in C (commie units)");
+		int temp = Convert.ToInt32(Console.ReadLine());
+		if(temp >= 30)
+		{
+			Console.WriteLine("Drink some water and dont stay outside too long!!!");
+		}
+		
+		if(temp < 30)
+		{
+			Console.WriteLine("Enjoy the good weather!");
+		}
+
+        Console.ReadLine();
+	}
+}
