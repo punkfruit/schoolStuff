7d2f3daa48068d4bc0739a4e4ac2fc0e8c7d58f5
punkfruit <thecybergrape@gmail.com>
2023-10-05 13:00:41 -0600

    challenge

--- SchoolProject/Assets/PlayerController.cs
+++ SchoolProject/Assets/PlayerController.cs
@@ -5,18 +5,27 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float crouchSpeed = 2.5f; 
+    public float sprintSpeed = 8f;   
     public float jumpForce = 5f;
     public float gravity = -40f;
     public float mouseSensitivity = 2f;
     public Transform playerCamera;
+    public float stamina = 100f; 
+    public float staminaDepletionRate = 20f; 
+    public float staminaRecoveryRate = 15f; 
+
     private Vector3 velocity;
     private bool isJumping = false;
+    private bool isCrouching = false; 
     private CharacterController controller;
     private float xRotation = 0f;
+    private float originalHeight; 
 
     private void Start()
     {
         controller = GetComponent<CharacterController>();
+        originalHeight = controller.height; 
         Cursor.lockState = CursorLockMode.Locked;
     }
 
@@ -25,8 +34,26 @@ public class PlayerController : MonoBehaviour
         float x = Input.GetAxisRaw("Horizontal");
         float z = Input.GetAxisRaw("Vertical");
 
-        Vector3 moveDirection = transform.right * x + transform.forward * z;
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            isCrouching = !isCrouching;
+            controller.height = isCrouching ? originalHeight / 2 : originalHeight;
+            moveSpeed = isCrouching ? crouchSpeed : 5f;
+        }
 
+        if (Input.GetKey(KeyCode.LeftShift) && !isCrouching && stamina > 0)
+        {
+            moveSpeed = sprintSpeed;
+            stamina -= staminaDepletionRate * Time.deltaTime;
+        }
+        else if (!isCrouching)
+        {
+            moveSpeed = 5f;
+            stamina += staminaRecoveryRate * Time.deltaTime;
+        }
+        stamina = Mathf.Clamp(stamina, 0, 100); 
+
+        Vector3 moveDirection = transform.right * x + transform.forward * z;
         Vector3 move = moveDirection.normalized * moveSpeed;
         controller.Move(move * Time.deltaTime);
 
@@ -38,17 +65,15 @@ public class PlayerController : MonoBehaviour
             velocity.y = -2f;
         }
 
-        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+        if (Input.GetButtonDown("Jump") && controller.isGrounded && !isCrouching)
         {
             velocity.y = Mathf.Sqrt(jumpForce * -2f * gravity);
         }
-
 
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
 
         transform.Rotate(Vector3.up * mouseX);
-
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
         playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
